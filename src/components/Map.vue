bf3481c9cd7de94c4396fc0ada99ec74d112ab5f
hivesbee <hivesbee@gmail.com>
2017-06-12 01:54:06 +0900

    initial commit

---
+++ src/components/Map.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="map">
+    <h1>{{ message }}</h1>
+    中心とする住所
+    <input type="text" />
+    <button type="button">検索</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'map',
+  data () {
+    return {
+      message: 'テスト文字列'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
